1e8765a92a4ff15404d7500acbc09601c216a67e
arthur <arthur.souza.reis222@gmail.com>
2023-12-26 16:57:18 -0300

    mais teste

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const server = express();
 
+const cursos = ["JavaScript", "React", "Typescript", "Nodejs", "Bootstrap", "AWS"];
 
 server.get("/teste", (req, res) => {
   const nome = req.query.nome;
@@ -12,5 +13,17 @@ server.get("/teste/:id", (req, res) => {
   const id = req.params.id;
   return res.json(`${id}`)
 })
+
+server.get("/curso/:index", (req, res) => {
+  const { index } = req.params;
+
+  return res.json(cursos[index])
+
+})
+
+
+
+
+
 
 server.listen(3000);
