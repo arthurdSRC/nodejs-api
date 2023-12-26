f55f8d47e853f17e3d234786c5e8f0aa775be4d8
arthur <arthur.souza.reis222@gmail.com>
2023-12-26 17:20:19 -0300

    CRUD

--- server.js
+++ server.js
@@ -4,25 +4,50 @@ const server = express();
 
 const cursos = ["JavaScript", "React", "Typescript", "Nodejs", "Bootstrap", "AWS"];
 
-server.get("/teste", (req, res) => {
-  const nome = req.query.nome;
-  return res.json(`${nome}`)
-})
-
-server.get("/teste/:id", (req, res) => {
-  const id = req.params.id;
-  return res.json(`${id}`)
-})
+server.use(express.json())
+//CRUD
 
 server.get("/curso/:index", (req, res) => {
   const { index } = req.params;
-
   return res.json(cursos[index])
 
 })
 
 
 
+// criar curso
+
+server.post("/curso", (req, res) => {
+  const { nome } = req.body;
+  cursos.push(nome);
+
+  return res.json({ message: "Curso adicionado com sucesso" })
+})
+
+
+// listar todos os cursos
+server.get("/curso", (req, res) => {
+  return res.json(cursos)
+})
+
+
+// atualizar um curso
+server.put("/curso/:index", (req, res) => {
+  const { index } = req.params;
+  const { nome } = req.body;
+  cursos[index] = nome;
+
+  return res.json({ message: "Curso atualizado com sucesso" })
+})
+
+// deletar Curso
+
+server.delete("/curso/:index", (req, res) => {
+  const { index } = req.params;
+  cursos.slice(index, 1);
+
+  return res.json({ message: "Curso deletado com sucesso" })
+})
 
 
 
